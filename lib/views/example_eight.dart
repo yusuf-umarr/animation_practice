a10399c061f28b0d59364cf6d39172e67f0954e3
mac <mac@macs-MacBook-Pro.local>
2023-04-02 14:25:30 +0100

    update: drawer animation completed

--- lib/views/example_eight.dart
+++ lib/views/example_eight.dart
@@ -13,7 +13,39 @@ class _ExampleEightState extends State<ExampleEight> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: MyDrawer(
+        drawer: Material(
+          child: Container(
+            color: const Color(0xff24283b),
+            child: ListView.builder(
+                padding: const EdgeInsets.only(top: 100, left: 120),
+                itemCount: 20,
+                itemBuilder: (BuildContext context, index) {
+                  return ListTile(
+                    title: Text(
+                      "Item $index",
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }),
+          ),
+        ),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Drawer",
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          body: Container(
+            color: Color(0xff414868),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -65,8 +97,8 @@ class _MyDrawerState extends State<MyDrawer> with TickerProviderStateMixin {
     );
 
     _yRotationAnimationForDrawer = Tween<double>(
-      begin: 0.0,
-      end: -pi / 2,
+      begin: -pi / 2.7,
+      end: 0.0,
     ).animate(_xControllerForDrawer);
   }
 
@@ -79,6 +111,50 @@ class _MyDrawerState extends State<MyDrawer> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidht = MediaQuery.of(context).size.width;
+    final maxDrag = screenWidht * 0.8;
+    return GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        final delta = details.delta.dx / maxDrag;
+        _xControllerForChild.value += delta;
+        _xControllerForDrawer.value += delta;
+      },
+      onHorizontalDragEnd: (details) {
+        if (_xControllerForChild.value < 0.5) {
+          _xControllerForChild.reverse();
+          _xControllerForDrawer.reverse();
+        } else {
+          _xControllerForChild.forward();
+          _xControllerForDrawer.forward();
+        }
+      },
+      child: AnimatedBuilder(
+          animation: Listenable.merge([
+            _xControllerForChild,
+            _xControllerForDrawer,
+          ]),
+          builder: (context, _) {
+            return Stack(
+              children: [
+                Container(),
+                Transform(
+                    alignment: Alignment.centerLeft,
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..translate(_xControllerForChild.value * maxDrag)
+                      ..rotateY(_yRotationAnimationForChild.value),
+                    child: widget.child),
+                Transform(
+                    alignment: Alignment.centerRight,
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..translate(
+                          -screenWidht + _xControllerForDrawer.value * maxDrag)
+                      ..rotateY(_yRotationAnimationForDrawer.value),
+                    child: widget.drawer),
+              ],
+            );
+          }),
+    );
   }
 }
